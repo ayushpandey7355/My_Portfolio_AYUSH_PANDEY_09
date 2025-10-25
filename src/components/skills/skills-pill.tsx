874260a3c5ef96498241b21e4984d3bcabbe31f3
ayushpandey7355 <ayushpandey7355@gmail.com>
2025-10-25 21:51:49 +0530

    Update

---
+++ src/components/skills/skills-pill.tsx
@@ -0,0 +1,17 @@
+import { FC, SVGProps } from "react";
+
+export type SkillPillProps = {
+  name: string;
+  icon?: FC<SVGProps<SVGSVGElement>> | null; // Make icon optional
+};
+
+export default function SkillPill(props: SkillPillProps) {
+  const { name, icon: Icon } = props;
+
+  return (
+    <div className="flex w-max items-center gap-2 overflow-hidden rounded-lg border border-accent/20 bg-white px-4 py-3 text-sm shadow-sm dark:bg-zinc-800 sm:text-base md:px-6 md:py-3 md:text-lg">
+      {Icon && <Icon className="h-5 w-5 sm:h-8 sm:w-8" />} {/* Only render if exists */}
+      <span className="font-medium">{name}</span>
+    </div>
+  );
+}
