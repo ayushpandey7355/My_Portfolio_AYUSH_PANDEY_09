de84d0a4695ba69b4a64896457f7179d7412bb3f
ayushpandey7355 <ayushpandey7355@gmail.com>
2025-10-26 08:27:01 +0530

    Fix heroProfile.png import for Next.js build

--- next.config.js
+++ next.config.js
@@ -11,7 +11,7 @@ const nextConfig = withBundleAnalyzer({
     dirs: ["src"],
   },
   images: {
-    domains: ["https://flagcdn.com"],
+    domains: ["flagcdn.com", "i.ibb.co"], // ✅ Added both hostnames here
   },
   webpack: (config) => {
     config.module.rules.push({
