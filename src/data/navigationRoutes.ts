baf5ed23d494f7167ba26a7244b0760cd7c5334b
ayushpandey7355 <ayushpandey7355@gmail.com>
2025-11-13 13:53:22 +0530

    fix

--- src/data/navigationRoutes.ts
+++ src/data/navigationRoutes.ts
@@ -4,8 +4,4 @@ export const routes: NavbarRoutes = [
   { title: "Home", href: "/" },
   { title: "About", href: "/about" },
   { title: "Projects", href: "/projects" },
-  {
-    title: "Resume",
-    href: "https://drive.google.com/file/d/1gEFTg2WwnHLGIYlKrZ_Rl0UAX6Bl7o0k/view?usp=drive_link",
-  },
 ];
